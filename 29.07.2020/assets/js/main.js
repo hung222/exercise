56082b3463a313ff313c7b475ed3716b7f419ec4
buihuongquynh <buigiangquynh@gmail.com>
2020-07-31 13:57:52 +0700

    f

---
+++ 29.07.2020/assets/js/main.js
@@ -0,0 +1,13 @@
+const header = {
+    init: function() {
+        this.clickShowMenu('.humbeger', '.temp', 'showMenu');
+    },
+    clickShowMenu: function(button, menu, ClassShowMenu) {
+        var btn = document.querySelector(button);
+        var NavMenu = document.querySelector(menu);
+        btn.addEventListener('click', () => {
+            NavMenu.classList.toggle(ClassShowMenu);
+        });
+    }
+}
+header.init();
